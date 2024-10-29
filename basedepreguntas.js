550236598e9b8cc6431ce4d252b9641efec8cb94
davidaliri <davidaliri95@gmail.com>
2024-10-29 13:04:12 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,152 @@
+basedepreguntas = [
+    {
+
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas.`,
+      imagen: `https://i.ibb.co/kmxtMF0/texto1-page-0001-1.jpg`,
+      respuesta: `	Cerrar las ventanillas del gallinero.	`,
+      distractores: [`	Cerrar la puerta de la cocina.	`, `	Apagar la linterna que llevaba.	`, `Quitar las botas en el lugar correcto.`],
+      
+      
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `2.- ¿De qué trata principal el discurso del Viejo Mayor? `,
+      respuesta: `		La opresión a los animales por los humanas.	`,
+      distractores: [`	La importancia de la unión de los animales.		`, `La necesidad constante de trabajar más duro.	 	`, `	Las diferentes habilidades de los animares.	`],
+      ayuda: ``,
+      ayudaImg: ``,
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `3.- ¿Cuál es el estado emocional que caracteriza a los animales mientras escuchan al viejo Mayor?`,
+      respuesta: `	Atentos y ansiosos por un cambio.		`,
+      distractores: [` Relajados y desinteresados al mensas. 		`, `	Indiferentes y desconfiados a las palabras.	`, `	Felices y despreocupados ante su situación.	`],	
+        
+        
+       
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `4.- Los animales de la granja estaban dispuestas a perder una hora de sueño, ¿cuál es tu opinión sobre la disposición de los animales?	`,
+      respuesta: `		Estoy de acuerdo con la decisión de los animales de oír la sabiduría del Viejo Mayor. 	`,
+      distractores: [`	Estoy de acuerdo con la forma en que el señor jones cuidaba a los animales de la granja.		`, `	No estoy de acuerdo con les quejas de los animales hacia el hombre que cuida de ellos. 			`, `	Creo que está bien una reunión de todos los animales para organizarse en el trabajo.			`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/HGZcGK0/texto2-page-0001.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas.`,
+      respuesta: `	Pasar un trapeador húmedo con limpiador detergente neutro diluido.	`,
+      distractores: [`	Utilizar una escobilla de fibra hasta que la superficie quede limpio.		`, `	Utilizar un limpiador abrasivo para eliminar la suciedad leve.		`, `	Dejar secar las manchas de forma natural y luego limpiar.	 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `6.-A partir del texto leído se puede concluir que:`,
+      respuesta: `	el tipo de limpieza depende del tipo de suciedad en pisos vinílicos.	`,
+      distractores: [`	la limpieza diaria es suficiente para mantener los pisos vinílicos.	`, `	la protección de pisos solo se logra con la limpieza profunda.	`, `	La colocación de felpudos en la entrada de la casa evita la suciedad.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `7.- En el siguiente fragmento nuevamente: La suciedad de los pisos generalmente proviene del exterior. La colocación de felpudos con fibras absorbentes en la parte externa del ingreso y en la parte interna de ingreso una alfombra de Nylon, o polipropileno que retenga el polvo, y las partículas abrasivas evitarán aproximadamente el 80% de la suciedad......................................................................................¿Cuál de las siguientes alternativas indica el significado de "felpudos"?`,
+      respuesta: `	Tela esponjosa`,
+      distractores: [`	PISO de madera		`, `	Alfombra de Nylon	`, `	Limpiador de superficies		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `8.- Lee nuevamente el siguiente fragmento del texto: Los soportes de caucho del mobiliario pueden dejar marcas en el suelo por lo que es recomendable incorporar protectores de plástico en las partes que tienen contacto con el piso para proteger la superficie..........................................,..................................................................................................¿Qué opinas sobre esta recomendación? `,
+      respuesta: `		Estoy de acuerdo con incorporar protectores de plástico para no dañar la superficie.	`,
+      distractores: [`	Me parece que es importante limpiar regularmente el mobiliario para evitar marcas.	`, `	Está bien porque el mobiliario no debe tener contacto con la superficie y evitar daños.		`, `	Está bien porque el mobiliario no debe tener contacto con la superficie y evitar daños.	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/HGTN5gm/texto-3-page-0001-1.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas. `,
+      respuesta: `	Privación de libertad y trato igualitario. 	 `,
+      distractores: [`	Multa económica y no discriminación por origen.			`, `	Sanción drástica y no agresión por raza. 		`, `	Advertencia verbal y prohibición de la discriminación.	 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `10.- ¿Cuál es el propósito del texto leído? `,
+      respuesta: `	Influir en los lectores para evitar la discriminación racial porque somos iguales.	`,
+      distractores: [`	Destacar que la discriminación se sanciona con la privación de la libertad.		`, `	Recordar el Día Internacional para la Eliminación de la Discriminación Racial.		`, `	Valorar la gran diversidad cultural y social en todo el territorio nacional.		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `11.- ¿Cuál es el tema abordado en el texto leído?`,
+      respuesta: `	La discriminación en nuestro país.	`,
+      distractores: [`	El inadecuado manejo de las emociones.	`, `	La vulneración de los derechos de la persona.`, `		La protección legal de los derechos.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `12.- Lee nuevamente el siguiente párrafo: Sin embargo, en pleno siglo XXI, cuando se creía que el racismo debía estar extinguido y que el Perú se iba asumiendo como un país pluricultural, el Ministerio de Cultura nos recuerda que el 53 % de la población considera que los peruanos son racistas o muy racistas; y que incluso un 8 % se percibe a sí mismo como tal, es decir, se perciben «racistas» o «muy racistas», conforme a los resultados de la primera encuesta nacional sobre Percepciones y Actitudes sobre Diversidad Cultural Discriminación Étnica-Racial.¿Qué opinas sobre más de 53 % de la población considera que los peruanos son racistas o muy racistas?`,
+      respuesta: `	Creo que está mal que faltemos al derecho de trato igualitario en nuestro país.	 	`,
+      distractores: [`	Creo que el Ministerio de Cultura del Perú ha querido mostrar su trabajo.		`, `	Parece que ha querido presentar las leyes contra la discriminación en el Perú.	 	`, `		No me parece que no se pueda valorar la diversidad cultural de nuestro país.		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/CJ6YBQK/texto-4-page-0001.jpg	`,
+      respuesta: `	Su tamaño reducido y emite luz.` ,
+      pregunta: `Lee el texto y  Responde las siguientes Pregutas		`,
+      distractores: [`	Su agresividad y voracidad incontrolado.		`, `	Su hábitat exclusivo en el Golfo de México.	`, `	Su preferencia por las aguas poco profundas.		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `14.- ¿De qué trata el texto leído?	`,
+      respuesta: ` El descubrimiento del tiburón enano americano.		`,
+      distractores: [`	El tamaño del tiburón enano americano.`, `	La producción del tiburón enano americano.		`, `	El hábitat del tiburón enano americano.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `15.- Lee el siguiente texto: El único antecedente similar registrado fue un pequeño marrajo capturado en el Océano Pacífico Oriental en 1979 y que se encuentra en el Museo Zoológico de San Petersburgo (Rusia)..................................................................Según el párrafo, ¿qué significa la palabra marrajo?	`,
+      respuesta: `	Tiburón.		`,
+      distractores: [`	Pez.		`, `	Animal.		`, `		Delfín.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `16.- En el siguiente fragmento del texto, ¿crees que el autor hace bien utilizar los paréntesis? .Los científicos del estudio, como decimos, han encontrado diferencias notables con el anterior 'tiburón luciérnaga', ya que presenta menos vértebras y numerosos fotóforos (órganos que emiten luz que se ven como puntos luminosos en la piel de los animales ).`,
+      respuesta: `	Parece que desea explicar el significado de la palabra fotóforos.		`,
+      distractores: [`	Creo que quiere resaltar la palabra fotóforos en el párrafo.	`, `	Opino que intenta expresar diferencias de otros tiburones.	`, `	Quiere describir cómo se produce la luminiscencia.	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/WBnKDNd/texto-5-page-0001.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Pregutas`,
+      respuesta: `		Cadáveres de vicuñas. 	`,
+      distractores: [`	Lobos marinos.	`, `	Resto de animales.	`, `	Aves muertas.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `18.- En la infografía se ha utilizado la palabra "envergadura" de la siguiente manera: "Miden 1,30 m (parado) y 3,50 m (envergadura alar)"..................................................... ¿Cuál de las siguientes palabras tiene el mismo sentido que "envergadura"?	`,
+      respuesta: `		Ancho.	`,
+      distractores: [`		Espesor. 		`, `	Altura			`, `	Volumen		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `19.- Según la última parte del texto leído, ¿cuál es la intención del autor?`,
+      respuesta: `		Alertar que el cóndor andino está en peligro de extinción.	`,
+      distractores: [`	Informar que el cóndor andino es parte de la riqueza natural.		`, `	Cuestionar su captura para la celebración de Yawar Fiesta. 	`, `		Denunciar el envenenamiento del cóndor andino por pobladores.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `20.- Según tu opinión, ¿para qué se ha incluido la imagen de Yawar Fiesta en el texto?`,
+      respuesta: `	Para indicar que la captura del cóndor andino es una amenaza, aunque sea para fines costumbristas.		`,
+      distractores: [`		Para resaltarla importancia del cóndor andino en celebraciones tradicionales como Yawar Fiesta.	`, `	Para prohibir a los comuneros la celebración tradicional del Yawar Fiesta en la zona andina.		`, `	Para que el texto escrito sea atractivo para los lectores y les invite a leer el contenido del texto.		`],
+    }
+    
+
+
+  ];
